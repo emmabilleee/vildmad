99e79d44debdb76affb937f2c629eb40e6df4377
Emma <emmabille16@gmail.com>
2023-09-07 16:21:02 +0200

    test af fetch

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
-fetch("url")
+fetch("https://aghzgepwumrefviuxsmp.supabase.co/rest/v1/vildmad", {
+  method: "GET",
+  headers: {
+    apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFnaHpnZXB3dW1yZWZ2aXV4c21wIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM3NDA3MTIsImV4cCI6MjAwOTMxNjcxMn0.IBRZrNvaKbyNmJxdxb9-DhSg6g1ynocm-TBz24Sj1PA",
+  },
+})
   .then((res) => res.json())
-  .then(showItems);
+  .then(showData);
 
-function showItems(items) {
-  items.forEach(showItem);
+function showData(items) {
+  console.log(items);
 }
 
-function showItem(item) {
-  console.log(item);
-  //Fang template
-  const template = document.querySelector("#item_boks").content;
+// function showItem(item) {
+//   console.log(item);
+//   //Fang template
+//   const template = document.querySelector("#item_boks").content;
 
-  //Lav en kopi
-  const copy = template.cloneNode(true);
+//   //Lav en kopi
+//   const copy = template.cloneNode(true);
 
-  //Ændre indhold
-  copy.querySelector("h3").textContent = item.navn;
-  copy.querySelector(".sankelandskab").textContent = item.navn;
-  copy.querySelector(".tilberedning").textContent = item.navn;
-  copy.querySelector(".måneder").textContent = item.navn;
-  copy.querySelector(".kategori").textContent = item.navn;
+//   //Ændre indhold
+//   copy.querySelector("h3").textContent = item.navn;
+//   copy.querySelector(".sankelandskab").textContent = item.navn;
+//   copy.querySelector(".tilberedning").textContent = item.navn;
+//   copy.querySelector(".måneder").textContent = item.navn;
+//   copy.querySelector(".kategori").textContent = item.navn;
 
-  //Append
-  document.querySelector("main").appendChild(copy);
-}
+//   //Append
+//   document.querySelector("main").appendChild(copy);
+// }
