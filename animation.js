4d48e271175168511114f5d65c5f8a7256c6d2aa
Hvidloegsida <idmi0001@stud.kea.dk>
2023-09-13 12:58:44 +0200

    animation

--- animation.js
+++ animation.js
@@ -1,19 +1,27 @@
 window.addEventListener("load", sidenVises);
-document.querySelector(".hero_skov").addEventListener("mouseover", hooverskov);
-document.querySelector(".hero_skov").addEventListener("mouseout", hooverskovreset);
+document.querySelector(".hero_skov").addEventListener("mouseenter", hooverskov);
+document.querySelector(".hero_skov").addEventListener("mouseleave", hooverskovreset);
 
 function sidenVises() {
   document.querySelector("#tale").classList.add("tale_ani");
+  document.querySelector("#tale").addEventListener("animationend", anireset);
+}
+
+function anireset() {
+  document.querySelector("#tale").classList.remove("tale_ani");
 }
 
 function hooverskov() {
   const tekst = document.querySelector("#tale");
   tekst.textContent = "SKOV-OMRÅDE";
+  tekst.classList.remove("tale_ani"); // Remove the animation class
+  tekst.classList.add("tale_ani"); // Add it immediatel
 }
 
 function hooverskovreset() {
-  const tekst = document.querySelector("#tale"); // Use #tale to select the element by its ID
-  tekst.textContent = "KLIK PÅ ET TERRÆN FOR AT SE MERE…";
+  const tekst = document.querySelector("#tale");
+  tekst.textContent = "KLIK PÅ ET OMRÅDE FOR AT SE MERE…";
+  tekst.classList.remove("tale_ani");
 }
 
 document.querySelector(".hero_vand").addEventListener("mouseover", hoovervand);
@@ -22,11 +30,14 @@ document.querySelector(".hero_vand").addEventListener("mouseout", hoovervandrese
 function hoovervand() {
   const tekst = document.querySelector("#tale");
   tekst.textContent = "VAND-OMRÅDE";
+  tekst.classList.remove("tale_ani"); // Remove the animation class
+  tekst.classList.add("tale_ani"); // Add it immediatel
 }
 
 function hoovervandreset() {
-  const tekst = document.querySelector("#tale"); // Use #tale to select the element by its ID
-  tekst.textContent = "KLIK PÅ ET TERRÆN FOR AT SE MERE…";
+  const tekst = document.querySelector("#tale");
+  tekst.textContent = "KLIK PÅ ET OMRÅDE FOR AT SE MERE…";
+  tekst.classList.remove("tale_ani");
 }
 
 document.querySelector(".hero_by").addEventListener("mouseover", hooverby);
@@ -35,11 +46,13 @@ document.querySelector(".hero_by").addEventListener("mouseout", hooverbyreset);
 function hooverby() {
   const tekst = document.querySelector("#tale");
   tekst.textContent = "BY-OMRÅDE";
+  tekst.classList.remove("tale_ani"); // Remove the animation class
+  tekst.classList.add("tale_ani"); // Add it immediatel}
 }
-
 function hooverbyreset() {
-  const tekst = document.querySelector("#tale"); // Use #tale to select the element by its ID
-  tekst.textContent = "KLIK PÅ ET TERRÆN FOR AT SE MERE…";
+  const tekst = document.querySelector("#tale");
+  tekst.textContent = "KLIK PÅ ET OMRÅDE FOR AT SE MERE…";
+  tekst.classList.remove("tale_ani");
 }
 
 document.querySelector(".hero_land").addEventListener("mouseover", hooverland);
@@ -48,9 +61,11 @@ document.querySelector(".hero_land").addEventListener("mouseout", hooverlandrese
 function hooverland() {
   const tekst = document.querySelector("#tale");
   tekst.textContent = "LAND-OMRÅDE";
+  tekst.classList.remove("tale_ani"); // Remove the animation class
+  tekst.classList.add("tale_ani"); // Add it immediatel}
 }
-
 function hooverlandreset() {
-  const tekst = document.querySelector("#tale"); // Use #tale to select the element by its ID
-  tekst.textContent = "KLIK PÅ ET TERRÆN FOR AT SE MERE…";
+  const tekst = document.querySelector("#tale");
+  tekst.textContent = "KLIK PÅ ET OMRÅDE FOR AT SE MERE…";
+  tekst.classList.remove("tale_ani");
 }
