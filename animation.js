c3f16cfca37fc62c9845ea5a8ceee48566d85395
Hvidloegsida <idmi0001@stud.kea.dk>
2023-09-14 11:45:35 +0200

    noter til animation.js

--- animation.js
+++ animation.js
@@ -2,15 +2,18 @@ window.addEventListener("load", sidenVises);
 document.querySelector(".hero_skov").addEventListener("mouseenter", hooverskov);
 document.querySelector(".hero_skov").addEventListener("mouseleave", hooverskovreset);
 
+/*ANIMATIONEN TALE_ANI SKAL STARTE I DET AT SIDEN VISES*/
 function sidenVises() {
   document.querySelector("#tale").classList.add("tale_ani");
   document.querySelector("#tale").addEventListener("animationend", anireset);
 }
-
+/*FJERNE ANIMATION SÅ AT DEN KAN GENBRUGES TIL SAMME ID SENERE 
+HEN*/
 function anireset() {
   document.querySelector("#tale").classList.remove("tale_ani");
 }
 
+/*HER ÆNDRES TEKSTEN I ID'EN TIL 'SKOV-OMRÅDE' NÅR MAN HOOVER OVER BILLEDET + TILFØJE ANIMATIONEN IGEN PÅ DEN NYE TEKST SELVOM AT DET ER DEN SAMME ID*/
 function hooverskov() {
   const tekst = document.querySelector("#tale");
   tekst.textContent = "SKOV-OMRÅDE";
@@ -18,6 +21,8 @@ function hooverskov() {
   tekst.classList.add("tale_ani"); // Add it immediatel
 }
 
+/*NÅR MAN IKKE HOOVER OVER BILLEDET SKAL TEKSTEN GÅ TILBAGE TIL AT VÆRE DEN ORIGINALE TEKT*/
+/*gentages på de næste elementer*/
 function hooverskovreset() {
   const tekst = document.querySelector("#tale");
   tekst.textContent = "KLIK PÅ ET OMRÅDE FOR AT SE MERE…";
